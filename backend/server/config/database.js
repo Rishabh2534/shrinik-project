c7aa1e3cd478ceaac86c6c2bb38fccdccc76960c
Rishabh2534 <145284089+Rishabh2534@users.noreply.github.com>
2025-02-09 11:37:54 +0530

    Update database.js

--- backend/server/config/database.js
+++ backend/server/config/database.js
@@ -9,9 +9,9 @@ Database user: sql12760643
  password: 7ZuJ1DgaPM
 */
      host: "sql12.freesqldatabase.com",   // Your MySQL Hostname
-    user: "sql12760643",              // Your MySQL Username
-    password: "7ZuJ1DgaPM",         // Your MySQL Password (keep it secret)
-    database: "sql12760643",  // Your MySQL Database Name
+    user: "sql12761841",              // Your MySQL Username
+    password: "C7MtNZ3gsl",         // Your MySQL Password (keep it secret)
+    database: "sql12761841",  // Your MySQL Database Name
     port: 3306  
    /* host: 'sql201.infinityfree.com', // Updated host
     user: 'if0_37994069',           // Updated username
