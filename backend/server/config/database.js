199e413a3f870647c2a453331f180a59b82926d4
Rishabh2534 <145284089+Rishabh2534@users.noreply.github.com>
2025-02-12 22:45:34 +0530

    Update database.js

--- backend/server/config/database.js
+++ backend/server/config/database.js
@@ -3,25 +3,13 @@
 import mysql from 'mysql';
 
 const connection= mysql.createConnection({
-  /*   Host: "sql12.freesqldatabase.com",
-Database name: sql12760643
-Database user: sql12760643
- password: 7ZuJ1DgaPM
-*/
+
      host: "sql12.freesqldatabase.com",   // Your MySQL Hostname
-    user: "sql12761841",              // Your MySQL Username
-    password: "C7MtNZ3gsl",         // Your MySQL Password (keep it secret)
+    user:  process.env.DBuser,              // Your MySQL Username
+    password: process.env.DBpassword,         // Your MySQL Password (keep it secret)
     database: "sql12761841",  // Your MySQL Database Name
     port: 3306  
-   /* host: 'sql201.infinityfree.com', // Updated host
-    user: 'if0_37994069',           // Updated username
-    password: 'Mishra@123',    // Updated password
-    database: 'if0_37994069_shrinik',   // Updated database name
-    port: 3306 
-   host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'shrinik'*/
+  
 });
 
 connection.connect((err) => {
