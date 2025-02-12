11b0000786f8332932cc941c8290b7d440826fd2
Rishabh2534 <145284089+Rishabh2534@users.noreply.github.com>
2025-02-12 22:43:00 +0530

    Update cloudinary.js

--- backend/server/config/cloudinary.js
+++ backend/server/config/cloudinary.js
@@ -2,9 +2,9 @@
 import { v2 as cloudinary } from 'cloudinary';
 
 cloudinary.config({
-  cloud_name:'dofi61euv',
-  api_key:'399984328245541',
-  api_secret:'IjVHnfrRrRw03GEY0m0j94HoIXg',
+  cloud_name:Process.env.cloud_name,//'',
+  api_key:Process.env.cloud_api_key,//'',
+  api_secret:Process.env.cloud_api_secret,//'',
 });
 
 export default cloudinary;
